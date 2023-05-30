c344a04f92901a66c1c0c79e70958ca2176781ec
xd.Yao <781144506@qq.com>
2023-05-31 00:07:59 +0800

    update

--- Server/Utility/UtiCommon.cs
+++ Server/Utility/UtiCommon.cs
@@ -2,6 +2,21 @@
 {
     public class UtiCommon
     {
+        public static bool CheckRepeat(List<int> list, int exceptid)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in list)
+            {
+                if (item == exceptid)
+                {
+                    continue;
+                }
 
+                if (set.Contains(item)) return false;
+                set.Add(item);
+            }
+
+            return true;
+        }
     }
 }
